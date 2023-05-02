2a85badcb477bdac34e43dec367e51910d3a53c5
haifeng2777 <709910630@qq.com>
2023-05-02 09:34:08 +0000

    更新 'js/100影院.js'

--- js/100影院.js
+++ js/100影院.js
@@ -3,7 +3,7 @@ muban.mxpro.二级.tabs = '.module-tab-items-box .module-tab-item';
 var rule={     
     title:'100影院',
     模板:'mxpro',
-    host:'https://www.dy111.me',
+    host:'http://www.100fyy3.com',
     url:'/vodshow/fyfilter.html',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
